e866464543ccbda155a7fe8f09e43807dd7515a2
Tensen <thienmpham@gmail.com>
2025-06-03 18:17:07 -0400

    Start get request using Bearer Token
    Bearer Token provides single authentication process

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function getMovieInfo() {
-
+function getRequest() {
+    //curl request GET using Bear token
+    curl--request GET \
+    --url 'https://api.themoviedb.org/3/movie/11' \
+    --header 'Authorization: Bearer '
 }
