03129a710ad8cfad1e1450f7ecc358677e16fec0
Tensen <thienmpham@gmail.com>
2025-06-10 18:38:05 -0400

    Try the fetch api
    Trying the fetch api. Realising that the fetch api
    is client side. I need to see if I can use the fetch
    api on the server side so I can use env variables
    /process.env

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 require('dotenv').config();
 
+
 const token = process.env.ACCESS_TOKEN;
-function getRequest() {
-    //curl request GET using Bear token
-    curl--request GET \
-    --url 'https://api.themoviedb.org/3/movie/11' \
-    --header`Authorization: Bearer ${token} `
+async function getRequest() {
+    //Fetch request using Bear token
+    const url = 'https://api.themoviedb.org/3/movie/11';
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "AUTHORIZATION": token,
+            },
+
+
+        });
+        if (!response.ok) {
+            throw new Error("Could not fetch resource!");
+        }
+        const data = await response.json;
+        console.log(data)
+    }
+    catch (error) {
+        console.error(error);
+    }
 
 
 }
+getRequest();
