5695cd190bbf998d954526ad2955c16e3ca9a3fa
Tensen <thienmpham@gmail.com>
2025-06-17 18:10:11 -0400

    Add type module
    Added type module to package.json to use ES

---
+++ server.js
@@ -0,0 +1,28 @@
+// require('dotenv').config();
+import fetch from 'node-fetch';
+
+const token = process.env.ACCESS_TOKEN;
+async function getRequest() {
+    //Fetch request using Bear token
+    const url = 'https://api.themoviedb.org/3/movie/11';
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                "AUTHORIZATION": token,
+            },
+
+        });
+        if (!response.ok) {
+            throw new Error("Could not fetch resource!");
+        }
+        const data = await response.json;
+        console.log(data)
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+
+}
+getRequest();
