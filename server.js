a8ccbaebd16afbf00262ca070d57be4173f619ca
Tensen <thienmpham@gmail.com>
2025-06-24 18:42:10 -0400

    Add import dotenv
    Process env token was being undefined due to not
    importing the dotenv module.

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 // require('dotenv').config();
 import fetch from 'node-fetch';
+import 'dotenv/config';
 
 const token = process.env.ACCESS_TOKEN;
 async function getRequest() {
     //Fetch request using Bear token
-    const url = 'https://api.themoviedb.org/3/movie/11';
-    try {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${token}`
-            }
+    const url = 'https://api.themoviedb.org/3/authentication';
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${token}`
         }
-        fetch(url, options)
-            .then(res => res.json())
-            .then(json => console.log(json))
-            .catch(err => console.error(err));
-    }
-    catch (error) {
-        console.error(error);
-    }
+    };
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error(err));
+
 
 
 }
