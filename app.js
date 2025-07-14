155df5bae58efa98581350f3ac38bdabcd118e09
Tensen <thienmpham@gmail.com>
2025-07-14 18:31:00 -0400

    Join app.js and trending.js
    Creating an endpoint to get data to the frontend.

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 import http from 'node:http';
 import 'dotenv/config';
 
-const server = http.createServer((req, res) => {
-    //Routing
-    if (req.url === '/trending') {
-        res.write(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello,world!');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Page not found');
-    }
-});
+function createServer() {
 
-const port = process.env.PORT;
-server.listen(port, () => {
-    console.log('Server listening');
-});
+
+    const server = http.createServer((req, res) => {
+        //Routing
+        if (req.url === '/trending') {
+            res.write(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello,world!');
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Page not found');
+        }
+    });
+
+    const port = process.env.PORT;
+    server.listen(port, () => {
+        console.log('Server listening');
+    });
+};
+createServer();
--- trending.js
+++ trending.js
@@ -29,3 +29,24 @@ async function getTrending() {
     // create endpoints to transfer data from backend to frontend
 }
 getTrending();
+
+function createServer() {
+
+
+    const server = http.createServer((req, res) => {
+        //Routing
+        if (req.url === '/trending') {
+            res.write(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello,world!');
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Page not found');
+        }
+    });
+
+    const port = process.env.PORT;
+    server.listen(port, () => {
+        console.log('Server listening');
+    });
+};
+createServer();
